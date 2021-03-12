234cda9a4eb29edb1ee78bbad468665710741814
Roman Kafturov <romkaft@mail.ru>
2021-03-12 21:41:27 +0300

    Inint project. Add webpack.config

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.css';
+
+const App = () => {
+	return (
+		<div className={styles.root}>
+			<h1>It's Books App!</h1>
+		</div>
+	);
+};
+
+export default App;
